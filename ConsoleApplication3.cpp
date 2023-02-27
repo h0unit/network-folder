a0609926bf7462c7ed0545a519754c069ea217d3
h0unit <banana.boy.bit@bk.ru>
2023-02-28 01:29:44 +0300

    f

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    double a = 0, b = 0;
+    char f;
+    do
+    {
+        if (a == 0)
+        {
+            cout << "Введите первое число: \n";
+            cin >> a;
+        }
+        cout << "Введите операцию: \n";
+        cin >> f;
+        if (f == 'Q')
+        {
+            cout << "Результат: " << a << "\n"; break;
+        }
+        if (f == 'C')
+        {
+            a = 0;
+            continue;
+        }
+        if (f == '+' || f == '-' || f == '*' || f == '/')
+        {
+            switch (f)
+            {
+            case '+':
+                cout << "Введите второе число:\n";
+                cin >> b;
+                a = a + b;
+                break;
+            case '-':
+                cout << "Введите второе число:\n";
+                cin >> b;
+                a = a - b;
+                break;
+            case '*':
+                cout << "Введите второе число:\n";
+                cin >> b;
+                a = a * b;
+                break;
+            case '/':
+                cout << "Введите второе число:\n";
+                cin >> b;
+                a = a / b;
+                break;
+            }
+            cout << "Результат = " << a << "\n";
+        }
+        else
+        {
+            cout << "Ошибка ввода, повторите ввод!\n";
+            continue;
+        }
+    }
+    while (f!='Q');
+    return 0;
+}
